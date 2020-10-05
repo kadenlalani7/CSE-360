bbdcff017007a06491af8ffdded7014681659ed9
kadenlalani7 <kadenlalani7@users.noreply.github.com>
2020-10-05 16:53:16 -0700

    Second version with example

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,7 +1,9 @@
-package cse360assignment02;
+package addMachine;
 
-public class AddingMachine {
+public class AddingMachine
+{
   private int total;
+  private String s = "0";
 
   public AddingMachine ()
   {
@@ -16,20 +18,34 @@ public class AddingMachine {
   public void add (int value)
   {
     total += value;
+    s = s + " + " + value;
   }
 
   public void subtract (int value)
   {
     total -= value;
+    s = s + " - " + value;
   }
 
   public String toString ()
   {
-    return "";
+    return s;
   }
 
   public void clear()
   {
     total = 0;
+  }
+
+  public static void main(String [] args)
+  {
+	  AddingMachine machine = new AddingMachine();
+  	  machine.add(3);
+  	  machine.subtract(1);
+  	  machine.add(12);
+  	  machine.subtract(8);
+  	  System.out.println(machine.toString());
+  	  System.out.print(machine.getTotal());
+
   }
 }
