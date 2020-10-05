7e9bccb8845f8e1bce2397fa4b6a32f3520dbad0
kadenlalani7 <kadenlalani7@users.noreply.github.com>
2020-10-05 15:19:14 -0700

    functions

--- AddingMachine.java
+++ AddingMachine.java
@@ -3,24 +3,33 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
 
-  public AddingMachine () {
+  public AddingMachine ()
+  {
     total = 0;  // not needed - included for clarity
   }
 
-  public int getTotal () {
-    return 0;
+  public int getTotal ()
+  {
+    return total;
   }
 
-  public void add (int value) {
+  public void add (int value)
+  {
+    total += value;
   }
 
-  public void subtract (int value) {
+  public void subtract (int value)
+  {
+    total -= value;
   }
 
-  public String toString () {
+  public String toString ()
+  {
     return "";
   }
 
-  public void clear() {
+  public void clear()
+  {
+    total = 0;
   }
 }
